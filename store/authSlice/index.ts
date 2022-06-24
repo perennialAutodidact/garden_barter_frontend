117924a841b71564bbee9f5e88aa24cb818883d0
Keegan Good <keegood8@gmail.com>
2022-06-24 00:06:33 -0700

    added resetAuthLoadingStatus, verify, refresh and fetchUser and edited state changes for other reducers

--- store/authSlice/index.ts
+++ store/authSlice/index.ts
@@ -1,90 +1,108 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User } from "../../ts/interfaces/auth";
 import { AuthState } from "../../ts/interfaces/auth";
-import { signup, login, logout, requestToken } from "./actions";
+import { signup, login, logout, verify, fetchUser, refresh } from "./actions";
 
 export const initialState: AuthState = {
   user: null,
   isAuthenticated: false,
-  authLoadingStatus: "PENDING",
-  accessToken: null
+  authLoadingStatus: "IDLE",
+  signupSuccess: false,
+  fetchUserSuccess: false,
 };
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    resetSignupSuccess(state) {
+      state.signupSuccess = false;
+    },
+    resetAuthLoadingStatus(state) {
+      state.authLoadingStatus = "IDLE";
+    }
+  },
   extraReducers: (builder) => {
     builder
+      // SIGNUP
       .addCase(signup.pending, (state) => {
-        state.accessToken = null;
-        state.user = null;
-        state.isAuthenticated = false;
         state.authLoadingStatus = "PENDING";
       })
       .addCase(signup.fulfilled, (state, action) => {
-        const { accessToken, user } = action.payload;
-        state.accessToken = accessToken;
-        state.user = user;
-        state.isAuthenticated = true;
+        state.signupSuccess = true;
         state.authLoadingStatus = "IDLE";
       })
       .addCase(signup.rejected, (state) => {
-        state.accessToken = null;
         state.user = null;
         state.isAuthenticated = false;
+        state.signupSuccess = false;
         state.authLoadingStatus = "IDLE";
       })
 
+      // LOGIN
       .addCase(login.pending, (state) => {
-        state.accessToken = null;
-        state.user = null;
         state.isAuthenticated = false;
         state.authLoadingStatus = "PENDING";
       })
       .addCase(login.fulfilled, (state, action) => {
-        const { accessToken, user } = action.payload;
-        state.accessToken = accessToken;
-        state.user = user;
         state.isAuthenticated = true;
         state.authLoadingStatus = "IDLE";
       })
       .addCase(login.rejected, (state) => {
-        state.accessToken = null;
+        state.isAuthenticated = false;
+        state.authLoadingStatus = "IDLE";
+      })
+
+      // VERIFY
+      .addCase(verify.pending, (state) => {
+        state.authLoadingStatus = "PENDING";
+      })
+      .addCase(verify.fulfilled, (state, action) => {
+        state.isAuthenticated = true;
+        state.authLoadingStatus = "IDLE";
+      })
+      .addCase(verify.rejected, (state) => {
         state.user = null;
         state.isAuthenticated = false;
         state.authLoadingStatus = "IDLE";
       })
 
-      .addCase(requestToken.pending, (state) => {
-        state.accessToken = null;
-        state.user = null;
-        state.isAuthenticated = false;
+      // REFRESH
+      .addCase(refresh.pending, (state) => {
         state.authLoadingStatus = "PENDING";
       })
-      .addCase(requestToken.fulfilled, (state, action) => {
-        const { accessToken, user } = action.payload;
-        state.accessToken = accessToken;
-        state.user = user;
-        state.isAuthenticated = true;
+      .addCase(refresh.fulfilled, (state) => {
         state.authLoadingStatus = "IDLE";
       })
-      .addCase(requestToken.rejected, (state) => {
-        state.accessToken = null;
-        state.user = null;
-        state.isAuthenticated = false;
+      .addCase(refresh.rejected, (state) => {
         state.authLoadingStatus = "IDLE";
+      })
+
+      // FETCH USER
+      .addCase(fetchUser.pending, (state) => {
+        state.authLoadingStatus = "PENDING";
+      })
+      .addCase(
+        fetchUser.fulfilled,
+        (state, action: PayloadAction<{ message: string; user: User }>) => {
+          console.log(action);
+          state.authLoadingStatus = "IDLE";
+          state.user = action.payload.user;
+          state.fetchUserSuccess = true;
+        }
+      )
+      .addCase(fetchUser.rejected, (state) => {
+        state.authLoadingStatus = "IDLE";
+        state.user = null;
       })
 
       // LOGOUT
       .addCase(logout.pending, (state) => {
-        state.accessToken = null;
         state.user = null;
         state.isAuthenticated = false;
         state.authLoadingStatus = "PENDING";
       })
-      .addCase(logout.fulfilled, (state, action) => {
-        state.accessToken = null;
-        state.user = null;
+      .addCase(logout.fulfilled, (state) => {
         state.isAuthenticated = false;
         state.authLoadingStatus = "IDLE";
       })
@@ -94,4 +112,5 @@ export const authSlice = createSlice({
   }
 });
 
+export const { resetSignupSuccess, resetAuthLoadingStatus } = authSlice.actions;
 export default authSlice.reducer;
