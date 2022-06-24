1b9d1317d527ea688d20f0920c58186bfc143748
Keegan Good <keegood8@gmail.com>
2022-06-24 00:02:32 -0700

    edited conditional rendering and added redirect if not authenticated

--- pages/barters/create.tsx
+++ pages/barters/create.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import BarterCreateForm from '../../components/Barters/BarterCreateForm'
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { useRouter } from "next/router";
+import BarterCreateForm from "../../components/Barters/BarterCreateForm";
 
 const BarterCreatePage = () => {
+  const router = useRouter();
+  const { isAuthenticated, authLoadingStatus } = useAppSelector(
+    state => state.auth
+  );
+
+  useEffect(
+    () => {
+      if (!isAuthenticated) {
+        router.push("/login?next=/barters/create/");
+      }
+    },
+    [isAuthenticated, authLoadingStatus]
+  );
+
   return (
     <div>
-        <BarterCreateForm />
+      {authLoadingStatus === "PENDING"
+        ? <div className="d-flex justify-content-center align-items-center vh-75">
+            <div
+              className="spinner-border text-success text-center mt-5"
+              role="status"
+            >
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        : <BarterCreateForm />}
     </div>
-  )
-}
+  );
+};
 
-export default BarterCreatePage
+export default BarterCreatePage;
