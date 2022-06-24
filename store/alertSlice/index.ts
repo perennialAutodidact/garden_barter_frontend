dfe7711acf63207ea6b6c35b35730a992b6f5436
Keegan Good <keegood8@gmail.com>
2022-06-23 23:59:32 -0700

    added type to alert state

--- store/alertSlice/index.ts
+++ store/alertSlice/index.ts
@@ -39,7 +39,7 @@ const alertSlice = createSlice({
     createAlert: (state:AlertState, action:PayloadAction<Alert>) => {
         state.alerts = state.alerts.concat(action.payload)
     },
-    deleteAlert: (state, action: PayloadAction<Alert>) => {
+    deleteAlert: (state:AlertState, action: PayloadAction<Alert>) => {
       state.alerts = state.alerts.filter(
         (alert) => alert.id !== action.payload.id
       );
