e5ae68817423fc17e4b51288117431fa81ad8576
Keegan Good <keegood8@gmail.com>
2022-06-23 23:28:21 -0700

    added docstring and handled blank strings

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,11 +1,19 @@
-export const titleize = (string) =>
+/**
+ * @param string string
+ * @returns string with the first letter of each word capitalized
+ */
+export const titleize = (string: string): string =>
   string
     .split(" ")
     .map((word, index) => {
       if (index !== 0) {
         return word.toLowerCase();
       } else {
-        return word[0].toUpperCase() + word.slice(1).toLowerCase();
+        if (word[0]) {
+          return word[0].toUpperCase() + word.slice(1).toLowerCase();
+        } else {
+          return "";
+        }
       }
     })
     .join(" ");
