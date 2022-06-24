2ec5100b32cde2bbbd1183dc7eedfddd83f90d7e
Keegan Good <keegood8@gmail.com>
2022-06-24 00:03:55 -0700

    removed GSSP & unneeded imports. added resetAuthLoadingStatus action

--- pages/barters/index.tsx
+++ pages/barters/index.tsx
@@ -1,39 +1,24 @@
 import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { GetServerSideProps, GetServerSidePropsResult } from "next";
 import { BartersHomePageProps } from "../../ts/interfaces/barters";
 import BarterList from "../../components/Barters/BarterList";
-import axios, { AxiosResponse } from "axios";
+import { resetAuthLoadingStatus } from "../../store/authSlice";
 
 const BartersHome = ({ barters }: BartersHomePageProps) => {
   const dispatch = useAppDispatch();
 
-  const { accessToken } = useAppSelector(state => state.auth);
+    useEffect(()=>{
+        if(dispatch && dispatch!==null&&dispatch!==undefined){
+            dispatch(resetAuthLoadingStatus())
+        }
+    },[dispatch])
 
   return (
-    <main className="container-fluid h-100-percent">
+    <main className="container-fluid">
       <BarterList barters={barters} />
     </main>
   );
 };
 
 
-export const getServerSideProps: GetServerSideProps = async (
-  context
-): Promise<GetServerSidePropsResult<BartersHomePageProps>> => {
-  try {
-    const res: AxiosResponse = await axios.get("/barters/", {});
-    
-    return {
-      props: {
-        barters: res.data.barters
-      }
-    };
-  } catch (err) {
-    console.log("barterSSProps", err);
-    return {
-      notFound: true
-    };
-  }
-};
 export default BartersHome;
