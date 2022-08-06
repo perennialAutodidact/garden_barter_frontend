869830c278c77d29ac96d5902290a4d5f9a58ed7
Keegan Good <keegood8@gmail.com>
2022-08-06 10:07:52 -0700

    added find and create

--- store/inboxSlice/actions.ts
+++ store/inboxSlice/actions.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 // import { BASE_URL } from "../../constants";
-
+import {MessageFormData} from  '../../ts/interfaces/inbox'
 const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
@@ -13,13 +13,38 @@ const headers = {
 };
 axios.defaults.withCredentials = true;
 
-
 export const fetchConversation = createAsyncThunk(
-    "auth/fetchConversation",
-    async (conversationId:number, { rejectWithValue }) => {
+  "inbox/fetchConversation",
+  async (conversationId: string| string[], { rejectWithValue }) => {
+    return await axios
+      .get(`/api/inbox/conversations/${conversationId}/`, { headers: headers })
+      .then((res) => res.data)
+      .catch((err) => rejectWithValue(err.response.data));
+  }
+);
+
+interface FindConversationParams {
+    senderId:number;
+    recipientId: number;
+    barterType: string;
+    barterId: string;
+}
+export const findConversation = createAsyncThunk(
+    "inbox/findConversation",
+    async (params:FindConversationParams, { rejectWithValue }) => {
       return await axios
-        .get(`/api/inbox/conversations/${conversationId}`, { headers: headers })
+        .get(`/api/inbox/conversations/find/`, { headers, params })
         .then((res) => res.data)
         .catch((err) => rejectWithValue(err.response.data));
     }
   );
+
+export const createMessage = createAsyncThunk(
+  "inbox/createMessage",
+  async (data: MessageFormData, { rejectWithValue }) => {
+    return await axios
+      .post(`/api/inbox/messages/create/`, data, { headers: headers })
+      .then((res) => res.data)
+      .catch((err) => rejectWithValue(err.response.data));
+  }
+);
