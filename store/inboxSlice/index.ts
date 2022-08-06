cd72faa0f1c8649c29614cea9cc9ae599a9c6be1
Keegan Good <keegood8@gmail.com>
2022-08-06 09:59:13 -0700

    added reducers for createMessage action

--- store/inboxSlice/index.ts
+++ store/inboxSlice/index.ts
@@ -1,17 +1,29 @@
 import { createSlice, PayloadAction, createAction } from "@reduxjs/toolkit";
 import { InboxState, Conversation } from "../../ts/interfaces/inbox";
+import { createMessage } from "./actions";
 
 export const initialState: InboxState = {
   conversations: [] as Conversation[],
   inboxLoadingStatus: "IDLE",
-  user: null
+  user: null,
+  activeConversation: null
 };
 
 const inboxSlice = createSlice({
   name: "inbox",
   initialState,
   reducers: {},
-  extraReducers: {}
+  extraReducers: (builder) => {
+    builder.addCase(createMessage.pending, (state) => {
+        state.inboxLoadingStatus = 'PENDING'
+    })
+    .addCase(createMessage.fulfilled, (state) => {
+        state.inboxLoadingStatus = 'IDLE'
+    })
+    .addCase(createMessage.rejected, (state) => {
+        state.inboxLoadingStatus = 'IDLE'
+    })
+  }
 });
 
 
