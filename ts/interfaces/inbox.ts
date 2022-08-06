c8dc29c8c4251e550d46890458ff8ddf7cb5cfb5
Keegan Good <keegood8@gmail.com>
2022-08-06 10:06:12 -0700

    added MessageFormData interface

--- ts/interfaces/inbox.ts
+++ ts/interfaces/inbox.ts
@@ -1,24 +1,33 @@
 import { User } from "./auth";
 
 export interface Message {
-    body: string;
-    dateRecieved: string;
-    sender: User;
-    recipient: User;
-    dateReceieved: string;
+  body: string;
+  dateRecieved: string;
+  sender: User;
+  recipient: User;
+  dateReceieved: string;
 }
 
 export interface Conversation {
-    id: number;
-    messages: Message[];
-    barterId: number;
-    barterType: "seed"|"plant"|"produce"|"material"|"tool";
-    sender: User;
-    recipient: User;
+  id: number;
+  messages: Message[];
+  barterId: number;
+  barterType: "seed" | "plant" | "produce" | "material" | "tool";
+  sender: User;
+  recipient: User;
 }
 
 export interface InboxState {
-    conversations: Conversation[]
-    inboxLoadingStatus: 'IDLE'|'PENDING';
-    user: User;
+  conversations: Conversation[];
+  inboxLoadingStatus: "IDLE" | "PENDING";
+  user: User;
+  activeConversation: Conversation;
+}
+
+export interface MessageFormData {
+  messageBody: string;
+  barterType: string;
+  barterId: string;
+  senderId: number;
+  recipientId: number;
 }
