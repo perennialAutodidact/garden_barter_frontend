6833e06b452f018d4ef501b05ae15294d1b73eed
Keegan Good <keegood8@gmail.com>
2022-08-06 09:03:08 -0700

    created interface for BarterHomePageProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import React, { useEffect } from "react";
 import BartersHome from "./barters";
 import { GetServerSideProps, GetServerSidePropsResult } from "next";
 import axios, { AxiosResponse } from "axios";
-import { Barter, BartersHomePageProps } from "../ts/interfaces/barters";
+import { AnyBarter, BartersHomePageProps } from "../ts/interfaces/barters";
 import { API_URL } from "../constants";
 
-const Index: React.FC = ({ barters }: { barters: Barter[] }) => {
+const Index: React.FC = ({ barters }: BartersHomePageProps) => {
   return <BartersHome barters={barters} />;
 };
 
