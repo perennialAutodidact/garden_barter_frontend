c19b3fbebed4497efc5b254fdc1c48f1413c14ee
KG <keegood8@gmail.com>
2022-08-22 14:28:10 -0700

    moved cookie logic into utils

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,23 +1,8 @@
 import cookie from "cookie";
+import { deleteTokenCookies } from "./utils";
 export default async (req, res) => {
   if (req.method === "POST") {
-
-    res.setHeader('Set-Cookie', [
-        cookie.serialize('access', '', {
-            httpOnly: true,
-            secure: process.env.NODE_ENV!=='development',
-            expires: new Date(0),
-            sameSite: 'strict',
-            path: '/api/'
-        }),
-        cookie.serialize('refresh', '', {
-            httpOnly: true,
-            secure: process.env.NODE_ENV!=='development',
-            expires: new Date(0),
-            sameSite: 'strict',
-            path: '/api/'
-        }),
-    ])
+    res = deleteTokenCookies(res)
 
     return res.status(200).json({message:'Logged out successfully.'})
 
