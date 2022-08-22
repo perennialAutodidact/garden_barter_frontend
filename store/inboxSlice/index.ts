35f2bfd4797679f5c3940a17eaeae0cc75337eed
KG <keegood8@gmail.com>
2022-08-22 14:32:14 -0700

    added findConversation reducer logic

--- store/inboxSlice/index.ts
+++ store/inboxSlice/index.ts
@@ -1,29 +1,44 @@
 import { createSlice, PayloadAction, createAction } from "@reduxjs/toolkit";
 import { InboxState, Conversation } from "../../ts/interfaces/inbox";
-import { createMessage } from "./actions";
+import { createMessage, findConversation } from "./actions";
 
 export const initialState: InboxState = {
-  conversations: [] as Conversation[],
-  inboxLoadingStatus: "IDLE",
-  user: null,
-  activeConversation: null
+    conversations: [] as Conversation[],
+    inboxLoadingStatus: "IDLE",
+    user: null,
+    activeConversation: null
 };
 
 const inboxSlice = createSlice({
-  name: "inbox",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(createMessage.pending, (state) => {
-        state.inboxLoadingStatus = 'PENDING'
-    })
-    .addCase(createMessage.fulfilled, (state) => {
-        state.inboxLoadingStatus = 'IDLE'
-    })
-    .addCase(createMessage.rejected, (state) => {
-        state.inboxLoadingStatus = 'IDLE'
-    })
-  }
+    name: "inbox",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(createMessage.pending, (state) => {
+                state.inboxLoadingStatus = 'PENDING'
+            })
+            .addCase(createMessage.fulfilled, (state) => {
+                state.inboxLoadingStatus = 'IDLE'
+            })
+            .addCase(createMessage.rejected, (state) => {
+                state.inboxLoadingStatus = 'IDLE'
+            })
+
+            // FIND CONVERSATION
+            .addCase(findConversation.pending, (state) => {
+                state.inboxLoadingStatus = 'PENDING'
+                state.activeConversation = null
+            })
+            .addCase(findConversation.fulfilled, (state, action) => {
+                state.inboxLoadingStatus = 'IDLE'
+                state.activeConversation = action.payload.conversation
+            })
+            .addCase(findConversation.rejected, (state) => {
+                state.inboxLoadingStatus = 'IDLE'
+                state.activeConversation = null
+            })
+    }
 });
 
 
