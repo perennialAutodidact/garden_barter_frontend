2c9366be1c745d85bb7084bca2b981c91053ddef
KG <keegood8@gmail.com>
2022-08-22 14:34:03 -0700

    moved cookie logic into utils

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,57 +1,43 @@
 import axios from "axios";
-import cookie from "cookie";
 import { API_URL } from "../../../common/constants";
+import { setTokenCookies } from "./utils";
 
 export default async (req, res) => {
-  if (req.method === "POST") {
-    const { email, password } = req.body;
+    if (req.method === "POST") {
+        const { email, password } = req.body;
 
-    try {
-      const apiRes = await axios.post(
-        `${API_URL}/token/`,
-        JSON.stringify({ email, password }),
-        {
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-          }
+        try {
+            const apiRes = await axios.post(
+                `${API_URL}/token/`,
+                JSON.stringify({ email, password }),
+                {
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json"
+                    }
+                }
+            );
+            const { access, refresh } = apiRes.data
+            res = setTokenCookies(res, access, refresh)
+            
+            return res.status(200).json({
+                message: 'Logged in successfully!'
+            })
+            
+        } catch (error) {
+            console.log(error)
+            if (error.response) {
+                return res.status(error.response.status).json({
+                    errors: [error.response.data.detail]
+                });
+            } else {
+                return res.status(500).json({
+                    errors: ["Something went wrong trying to log in."]
+                });
+            }
         }
-      );
-
-      res.setHeader("Set-Cookie", [
-        cookie.serialize("access", apiRes.data.access, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV !== "development",
-          maxAge: 60 * 30,
-          sameSite: "strict",
-          path: "/api/"
-        }),
-        cookie.serialize("refresh", apiRes.data.refresh, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV !== "development",
-          maxAge: 60 * 60 * 24,
-          sameSite: "strict",
-          path: "/api/"
-        })
-      ]);
-
-      return res.status(200).json({
-        message: 'Logged in successfully!'
-      })
-    } catch (error) {
-        // console.log(error)
-      if (error.response) {
-        return res.status(error.response.status).json({
-          errors: [error.response.data.detail]
-        });
-      } else {
-        return res.status(500).json({
-          errors: ["Something went wrong trying to log in."]
-        });
-      }
+    } else {
+        res.setHeader("Allow", ["POST"]);
+        return res.status(405).json({ errors: [`Method ${req.method} not allowed`] });
     }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    return res.status(405).json({ errors: [`Method ${req.method} not allowed`] });
-  }
 };
