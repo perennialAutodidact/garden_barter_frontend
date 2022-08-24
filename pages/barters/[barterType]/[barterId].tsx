b8f435c52aeefd1fefeba084734237ed592d91c4
KG <keegood8@gmail.com>
2022-08-24 12:23:44 -0700

    added type to GSSP context

--- pages/barters/[barterType]/[barterId].tsx
+++ pages/barters/[barterType]/[barterId].tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import axios from "axios";
 import BarterDetail from "../../../components/Barters/BarterDetail";
 import { API_URL } from "../../../common/constants";
@@ -6,34 +6,35 @@ import { Barter } from "../../../ts/interfaces/barters";
 import { useRouter, NextRouter } from "next/router";
 import RouteProtector from "../../../common/components/RouteProtector";
 interface BarterDetailPageProps {
-  barter: Barter;
+    barter: Barter;
 }
 
 const BarterDetailPage = ({ barter }: BarterDetailPageProps) => {
-  const router: NextRouter = useRouter();
-  return (
-      <div className="container-fluid bg-light-lighter">
-        <BarterDetail barter={barter} />
-      </div>
-  );
+    const router: NextRouter = useRouter();
+    return (
+        <div className="container-fluid bg-light-lighter">
+            <BarterDetail barter={barter} />
+        </div>
+    );
 };
-export const getServerSideProps: GetServerSideProps = async context => {
-  let { barterType, barterId } = context.query;
-  console.log(barterType, barterId)
+export const getServerSideProps: GetServerSideProps =
+    async (context: GetServerSidePropsContext) => {
+        let { barterType, barterId } = context.query;
+        console.log(barterType, barterId)
 
-  try {
-    const res = await axios.get(`${API_URL}/barters/${barterType}/${barterId}`);
+        try {
+            const res = await axios.get(`${API_URL}/barters/${barterType}/${barterId}`);
 
-    return {
-      props: {
-        barter: res.data.barters[0]
-      }
+            return {
+                props: {
+                    barter: res.data.barters[0]
+                }
+            };
+        } catch (error) {
+            return {
+                notFound: true
+            };
+        }
     };
-  } catch (error) {
-    return {
-      notFound: true
-    };
-  }
-};
 
 export default BarterDetailPage;
