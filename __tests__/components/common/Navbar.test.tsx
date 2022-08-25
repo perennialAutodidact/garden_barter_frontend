ffa4075692b98b0ba9df7ec6843d4a614e722f8d
keegangood <keegood8@gmail.com>
2022-08-25 13:49:12 -0700

    added navbar tests

---
+++ __tests__/components/common/Navbar.test.tsx
@@ -0,0 +1,90 @@
+import Navbar from "../../../common/components/Layout/Navbar/Navbar";
+import { render, RenderResult, screen } from "../../utils/utils";
+import userEvent from "@testing-library/user-event";
+import { RootState, initialState as rootState } from "../../../store/store";
+import {
+  TEST_USER,
+  INIT_STATE_NOT_AUTHENTICATED,
+  INIT_STATE_AUTHENTICATED,
+} from "../../testData";
+import { NextRouter } from "next/router";
+import { createMockRouter } from "../../utils/createMockRouter";
+
+const setupNavbar = (
+  initialState: RootState = rootState,
+  router: Partial<NextRouter>
+) =>
+  render(<Navbar />, {
+    initialState,
+    router,
+  });
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: {},
+      asPath: "",
+      push: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+      },
+      beforePopState: jest.fn(() => null),
+      prefetch: jest.fn(() => null),
+    };
+  },
+}));
+const useRouter = jest.spyOn(require("next/router"), "useRouter");
+
+describe("<Navbar/>", () => {
+
+  it("should render without crashing", () => {
+    setupNavbar(rootState, createMockRouter());
+  });
+
+  it("should render unauthenticated navbar", async () => {
+    const { getByRole, findByText } = setupNavbar(
+      INIT_STATE_NOT_AUTHENTICATED,
+      createMockRouter()
+    );
+
+    expect(getByRole("GBLogo")).toBeInTheDocument();
+    expect(getByRole("NavbarHeader")).toBeInTheDocument();
+
+    expect(await findByText(/sign up/i)).toBeInTheDocument();
+    expect(await findByText(/log in/i)).toBeInTheDocument();
+  });
+
+  it("should render authenticated navbar", async () => {
+    const user = userEvent.setup();
+
+    const { getByRole, findByRole,queryByRole, findByText, debug } = setupNavbar(
+        {
+            ...rootState,
+            auth: {
+                ...rootState.auth,
+                authLoadingStatus: "IDLE" as "IDLE",
+                isAuthenticated: true,
+                user: TEST_USER
+            },
+        },
+        createMockRouter()
+        );
+        await findByRole('AccountDropdown')
+        screen.debug()
+        // const accountDropdown = await screen.findByRole("AccountDropdown");
+        // expect(getByRole("GBLogo")).toBeInTheDocument();
+        // expect(getByRole("NavbarHeader")).toBeInTheDocument();
+    // accountDropdown.onclick = jest.fn();
+    // expect(accountDropdown).toBeInTheDocument();
+    // await user.click(accountDropdown);
+    // expect(await findByRole("AccountDropdownMenu")).toBeInTheDocument();
+
+    // expect(await findByRole("CreateBarterButton")).toBeInTheDocument();
+    // expect(await findByText(TEST_USER.username)).toBeInTheDocument();
+  });
+
+  it("should render all account dropdown buttons and they should navigate to the correct pages", () => {});
+});
--- common/components/Layout/Navbar/Navbar.tsx
+++ common/components/Layout/Navbar/Navbar.tsx
@@ -1,30 +1,26 @@
-import React from "react";
+import { unwrapResult } from "@reduxjs/toolkit";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import Image from "next/image";
+import React from "react";
+import { logout } from "../../../../store/authSlice/actions";
+import { useAppDispatch, useAppSelector } from "../../../../store/hooks";
 import AuthLinks from "./AuthLinks";
 import GuestLinks from "./GuestLinks";
-import { useAppSelector, useAppDispatch } from "../../../../store/hooks";
-import { logout } from "../../../../store/authSlice/actions";
-import { unwrapResult } from "@reduxjs/toolkit";
-import { CgAdd } from "react-icons/cg";
-import Link from "next/link";
 
 function GBNavbar() {
   const router = useRouter();
   const dispatch = useAppDispatch();
-  const {
-    isAuthenticated,
-    authLoadingStatus,
-    user,
-  } = useAppSelector(state => state.auth);
+  const { isAuthenticated, authLoadingStatus, user } = useAppSelector(
+    (state) => state.auth
+  );
 
   const handleLogout = (event: React.MouseEvent<HTMLButtonElement>) => {
     dispatch(logout())
       .then(unwrapResult)
-      .then(res => {
+      .then((res) => {
         router.push("/");
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -37,50 +33,57 @@ function GBNavbar() {
               src="/images/garden_barter_logo.svg"
               className="navbar-logo px-lg-3"
               alt="Garden Barter Logo"
+              role="GBLogo"
             />
-            <h1 className="d-none d-lg-block navbar-brand-heading text-success m-0">
-              Garden Barter
-            </h1>
-            <h2 className="d-lg-none navbar-brand-heading text-success m-0">
-              Garden Barter
-            </h2>
+            <div role="NavbarHeader">
+              <h1 className="d-none d-lg-block navbar-brand-heading text-success m-0">
+                Garden Barter
+              </h1>
+              <h2 className="d-lg-none navbar-brand-heading text-success m-0">
+                Garden Barter
+              </h2>
+            </div>
           </a>
         </Link>
-        {authLoadingStatus === "PENDING" && !isAuthenticated
-          ? <div
-              className="navbar-nav spinner-border text-success d-lg-none"
-              role="status"
-            >
-              <span className="visually-hidden">Loading...</span>
-            </div>
-          : // HAMBURGER
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>}
+        {authLoadingStatus === "PENDING" && !isAuthenticated ? (
+          <div
+            className="navbar-nav spinner-border text-success d-lg-none"
+            role="status"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        ) : (
+          // HAMBURGER
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+        )}
 
         {/* NAV LINKS */}
         <div
           className="collapse navbar-collapse d-lg-flex justify-content-lg-end align-items-lg-center"
           id="navbarNav"
         >
-          {authLoadingStatus === "PENDING"
-            ? <div
-                className="navbar-nav spinner-border text-success"
-                role="status"
-              >
-                {""}
-              </div>
-            : isAuthenticated
-              ? <AuthLinks handleLogout={handleLogout} />
-              : <GuestLinks />}
+          {authLoadingStatus === "PENDING" ? (
+            <div
+              className="navbar-nav spinner-border text-success"
+              role="status"
+            >
+              {""}
+            </div>
+          ) : isAuthenticated && user ? (
+            <AuthLinks user={user} handleLogout={handleLogout} />
+          ) : (
+            <GuestLinks />
+          )}
         </div>
       </div>
     </nav>
