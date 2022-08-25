95c7020c45a466f18d35bac4c984d1bb2de52bdf
KG <keegood8@gmail.com>
2022-08-24 21:46:40 -0700

    hooked up IconButton to go to fetched conversation or new conversation

--- components/Barters/BarterDetail.tsx
+++ components/Barters/BarterDetail.tsx
@@ -1,85 +1,83 @@
 import React, { useState } from "react";
 import { Barter } from "../../ts/interfaces/barters";
 import { humanizeDateDistance } from "./utils";
-import IconButton from "./IconButton";
+import IconButton from "../../common/components/IconButton";
 import BarterIcon from "./BarterIcon";
 import { useAppSelector } from "../../store/hooks";
 import Link from "next/link";
 import { FaRegEnvelope } from "react-icons/fa";
 import { useRouter, NextRouter } from "next/router";
 import { useFetchConversation } from "../Inbox/hooks/useFetchConversation";
+import { Conversation } from '../../ts/interfaces/inbox'
 interface BarterDetailProps {
-  barter: Barter;
+    barter: Barter;
 }
 
 const BarterDetail = ({ barter }: BarterDetailProps) => {
-  const router: NextRouter = useRouter();
-  const [dateCreated, _] = useState<string>(
-    humanizeDateDistance(barter.dateCreated.toString())
-  );
-  const { user } = useAppSelector((state) => state.auth);
+    const router: NextRouter = useRouter();
+    const [dateCreated, _] = useState<string>(
+        humanizeDateDistance(barter.dateCreated.toString())
+    );
+    const { user } = useAppSelector((state) => state.auth);
 
-  const conversation = useFetchConversation(user, barter);
+    const conversation: Conversation = useFetchConversation(user, barter);
 
-  console.log("conversation", conversation);
-  console.log("user", user);
+    console.log("conversation", conversation);
+    console.log("user", user);
 
-  return (
-    <div className="row p-lg-4 mb-5">
-      <div className="col-12 col-lg-8 py-3 bg-light-dark d-flex flex-column align-items-center border">
-        <img src="https://picsum.photos/300" alt="" className="rounded" />
-        <div className="mt-2 mb-5 d-flex justfiy-content-center bg-light rounded">
-          <div className="p-2 border-bottom d-flex justify-content-evenly gap-3">
-            <img src="https://picsum.photos/50" className="rounded" alt="" />
-            <img src="https://picsum.photos/50" className="rounded" alt="" />
-            <img src="https://picsum.photos/50" className="rounded" alt="" />
-          </div>
+    const goToConversation = (conversation: Conversation) => {
+        if (router && router !== undefined && router !== null) {
+            router.push({
+                pathname: '/inbox/conversations/[[...conversationId]]/',
+                query: { conversationId: conversation ? conversation.id : 'new' }
+            })
+        }
+    }
+
+    return (
+        <div className="row p-lg-4 mb-5">
+            <div className="col-12 col-lg-8 py-3 bg-light-dark d-flex flex-column align-items-center border">
+                <img src="https://picsum.photos/300" alt="" className="rounded" />
+                <div className="mt-2 mb-5 d-flex justfiy-content-center bg-light rounded">
+                    <div className="p-2 border-bottom d-flex justify-content-evenly gap-3">
+                        <img src="https://picsum.photos/50" className="rounded" alt="" />
+                        <img src="https://picsum.photos/50" className="rounded" alt="" />
+                        <img src="https://picsum.photos/50" className="rounded" alt="" />
+                    </div>
+                </div>
+                <h3 className="h2 mt-2 mb-2 w-100 d-none d-lg-block">Description</h3>
+                <div
+                    className="my-1 pb-3 w-100 border-bottom d-none d-lg-block"
+                    data-testid="BarterDescription"
+                >
+                    {barter.description}
+                </div>
+            </div>
+
+            <div className="col-12 col-lg-4 p-3 border-top border-bottom border">
+                <BarterIcon barterType={barter.barterType} />
+
+                <h3 className="h2 mt-2 mb-2">{barter.title}</h3>
+                <p className="mb-lg-3">
+                    Posted {dateCreated} by {barter.creator.username || "Anonymous"}
+                </p>
+
+                <h3 className="h2 mt-2 mb-2 w-100 d-block d-lg-none">Description</h3>
+                <div
+                    className="my-1 pb-3 w-100 border-bottom d-block d-lg-none"
+                    data-testid="BarterDescription"
+                >
+                    {barter.description}
+                </div>
+                <h3 className="h3 my-2 ">Trade</h3>
+                <p className="mb-lg-5">
+                    {barter.isFree ? "Free (no trade required)" : barter.willTradeFor}
+                </p>
+
+                <IconButton icon={<FaRegEnvelope onClick={() => goToConversation(conversation)} />} />
+            </div>
         </div>
-        <h3 className="h2 mt-2 mb-2 w-100 d-none d-lg-block">Description</h3>
-        <div
-          className="my-1 pb-3 w-100 border-bottom d-none d-lg-block"
-          data-testid="BarterDescription"
-        >
-          {barter.description}
-        </div>
-      </div>
-
-      <div className="col-12 col-lg-4 p-3 border-top border-bottom border">
-        <BarterIcon barterType={barter.barterType} />
-
-        <h3 className="h2 mt-2 mb-2">{barter.title}</h3>
-        <p className="mb-lg-3">
-          Posted {dateCreated} by {barter.creator.username || "Anonymous"}
-        </p>
-
-        <h3 className="h2 mt-2 mb-2 w-100 d-block d-lg-none">Description</h3>
-        <div
-          className="my-1 pb-3 w-100 border-bottom d-block d-lg-none"
-          data-testid="BarterDescription"
-        >
-          {barter.description}
-        </div>
-        <h3 className="h3 my-2 ">Trade</h3>
-        <p className="mb-lg-5">
-          {barter.isFree ? "Free (no trade required)" : barter.willTradeFor}
-        </p>
-
-        <Link
-          href={{
-            // pathname: "/inbox/conversations/[conversationId]/",
-            pathname: "/",
-            query: {
-              conversationId: conversation ? conversation.id : "",
-            },
-          }}
-          as="/inbox/conversations/send-message/"
-          passHref
-        >
-          <IconButton icon={<FaRegEnvelope />} />
-        </Link>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default BarterDetail;
